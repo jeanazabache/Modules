6ca553f899f7d08f6a12f30bb13142c1a6dba81a
Jean Azabache Medina <jazabachem1@gmail.com>
2024-07-08 00:08:45 -0500

    update

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,10 +1,8 @@
-################## Region AWS ##################
 variable "region" {
   description = "The AWS region to create the VPC in"
   type        = string
 }
 
-################## Variable CIDR VPC ##################
 variable "vpc_cidr_block" {
   description = "The CIDR block for the VPC"
   type        = string
@@ -15,7 +13,6 @@ variable "vpc_name" {
   type        = string
 }
 
-################## Count Subnet ##################
 variable "subnet_count_public" {
   description = "Number of subnets to create"
   type        = number
@@ -26,13 +23,11 @@ variable "subnet_count_private" {
   type        = number
 }
 
-################## Availability Zone ##################
 variable "availability_zones" {
   description = "List of availability zones for the subnets"
   type        = list(string)
 }
 
-################## Variables CIDR ##################
 variable "subnets_cidr_public" {
   description = "Lista de cidrs para subnets publicos"
   type        = list(string)
@@ -41,10 +36,4 @@ variable "subnets_cidr_public" {
 variable "subnets_cidr_private" {
   description = "Lista de cidrs para subnets publicos"
   type        = list(string)
-}
-
-################## Security Group ##################
-variable "name_security_group" {
-  description = "Name security group"
-  type = string
 }
